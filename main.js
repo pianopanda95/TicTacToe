2dcbb4cddbef8ad5b850ae96bd4be5a446870f20
pianopanda95 <kramerfanni95@gmail.com>
2022-07-19 15:11:49 +0200

    add checkState and createPopup functions

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const playersFieldset = [...document.querySelectorAll('nav#players > fieldset')]
 const inputs = [...document.querySelectorAll('input[id^=player]')];
 const startBtn = document.querySelector('button#start');
 
-
 const markers = ['X', 'O']
+
 startBtn.addEventListener('click', () => {
     if (!inputs.every(inp => inp.value))
         return;
@@ -23,6 +23,9 @@ startBtn.addEventListener('click', () => {
         playersFieldset[index].appendChild(playerMarker);
     });
 
+    const randInd = (() => Math.floor(Math.random() * 2))();
+    playersFieldset[randInd].classList.add('active')
+
     startBtn.remove();
     startGame();
 });
@@ -37,18 +40,59 @@ const startGame = () => {
     }
 
     const gameboard = document.querySelector('main#gameboard');
-    const gameboardArray = Array(9).fill('', 0);
 
-    gameboardArray.map(x => {
+    const gameboardArray =
+        Array(9).fill('').map((x, i) => {
         const field = document.createElement('button');
         field.classList.add('field');
         gameboard.appendChild(field);
-        field.addEventListener('click', () => {
-            const marker = document.querySelector('fieldset.active > p.player-marker').textContent;
+
+        field.addEventListener('click', (e) => {
+            const marker = document.querySelector('fieldset.active > p.player-marker')
+                          .textContent;
             field.innerText = marker;
-            toggleActivePlayer();
             field.setAttribute('disabled', 'disabled')
+            gameboardArray[i] = field.innerText;
+            checkState(marker, gameboardArray)
+            toggleActivePlayer();
         });
+        return x;
     });
 
+    const createPopUp = name => {
+        const popUp = document.createElement('div');
+        popUp.classList.add('pop-up');
+        popUp.textContent = name? `CONGRATS ${name}! You won!` : `It's a tie, no winners...`
+        gameboard.appendChild(popUp);
+        toggleActivePlayer();
+    }
+    
+
+    const checkState = (marker, gameboardArray) => {
+    
+        const indexesOfSamePlayersTiles =
+            gameboardArray.map((content, index) => content === marker ? index : '' )
+                            .filter(item => item !== '')
+                            .join('');
+    
+        const winnerCases = [ /012/, /345/, /678/,
+                                /.*0.*3.*6/, /.*1.*4.*7/, /.*2.*5.*8/,
+                                /.*0.*4.*8/, /.*2.*4.*6/ ];
+    
+        if (winnerCases.some(regex => regex.test(indexesOfSamePlayersTiles))){
+            const currentName = document.querySelector('fieldset.active > p.player-name')
+                               .textContent;
+            createPopUp(currentName);
+        }
+        else if (gameboardArray.filter(item => item !== '').length === 9)
+            createPopUp();
+    }
+
 }
+
+
+
+
+
+
+
